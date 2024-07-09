b531d5fe351d463775075613677e83d2eb050a95
Mei Zhu <maggiez0613@gmail.com>
2024-07-09 00:38:50 -0400

    hw done

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -1,7 +1,15 @@
 import getId from "../utils/getId";
 
 class CartItem {
-
+  #id = getId();
+  constructor (name, price) {
+    this.id = this.#id;
+    this.name = name;
+    this.price = price; 
+  }
+  getId() {
+    return this.#id;
+  }
 }
 
 export default CartItem;
--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,40 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
+  id = getId();
+  #cartItems = [];
+  static #allCarts = [];
 
+  constructor() {
+    ShoppingCart.#allCarts.push(this);
+  }
+
+  createItem(name, price) {
+    const item = new CartItem(name, price);
+    this.#cartItems.push(item);
+    return item; 
+  }
+  getItems(){
+    return [...this.#cartItems];
+  }
+  removeItem(id){
+    this.#cartItems.splice(this.#cartItems.findIndex((item) => item.getId() === id), 1);
+  }
+  getTotal(){
+    return this.#cartItems.reduce((total, item) => total + item.price, 0);
+    // return this.#cartItems.reduce((total, { price }) => total + price, 0);
+    // let total = 0;
+    // for (let i = 0; i < this.#cartItems.length; i++) {
+    //   total += this.#cartItems[i].price;
+    // }
+    // return total;
+  }
+  static listAll() {
+    return [...ShoppingCart.#allCarts];
+  }
+  static findBy(id) {
+    return ShoppingCart.#allCarts.find((cart) => cart.id === id);
+  }
 }
 
 export default ShoppingCart;
